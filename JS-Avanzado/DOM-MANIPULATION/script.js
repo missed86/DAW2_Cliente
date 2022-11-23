a38088ef8fe7286ba57312f55ccd8ce89d0bd0cc
missed86 <jimmy11286@gmail.com>
2022-11-23 10:52:31 +0100

    dsadsa

---
+++ JS-Avanzado/DOM-MANIPULATION/script.js
@@ -0,0 +1,37 @@
+const root = document.getElementById('root')
+
+// Crear elementos
+const p = document.createElement('p')
+p.textContent = 'Esto está rellenado desde JS'
+root.appendChild(p)
+
+
+// Borrar elementos
+const borrame = document.createElement('p')
+borrame.textContent = 'borrame'
+root.appendChild(borrame)
+
+root.removeChild(borrame)
+
+//Pruebas con bucles
+// for (let i = 65; i < 91; i++) {
+//   const char = String.fromCharCode(i)
+//   const btn = document.createElement('button')
+//   btn.textContent = char
+//   root.appendChild(btn)
+// }
+
+//Fragmentos de nodos
+const fragment = document.createDocumentFragment()
+for (let i = 65; i < 91; i++) {
+  const char = String.fromCharCode(i)
+  const btn = document.createElement('button')
+  btn.textContent = char
+  btn.id = char
+  btn.className = 'char'
+  btn.classList.add('char',char)
+  btn.dataset.char = char
+  fragment.appendChild(btn)
+}
+
+root.appendChild(fragment)
