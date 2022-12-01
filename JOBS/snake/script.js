a832184fc873bdb757026b7caef76a9efa85c470
missed86 <jimmy11286@gmail.com>
2022-12-01 18:24:11 +0100

    snake mola

--- JOBS/snake/script.js
+++ JOBS/snake/script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext("2d")
 
-const velocity = 1000;
+const velocity = 300;
 
 //Movimiento mediante teclado
 const Keys = {
@@ -12,23 +12,34 @@ const Keys = {
 };
 
 document.body.addEventListener('keydown', (e)=>{
-    if(e.key == 'ArrowUp') Keys.up = true;
-    if(e.key == 'ArrowDown') Keys.down = true;
-    if(e.key == 'ArrowLeft') Keys.left = true;
-    if(e.key == 'ArrowRight') Keys.right = true;
-}) 
-document.body.addEventListener('keyup', (e)=>{
-    if(e.key == 'ArrowUp') Keys.up = false;
-    if(e.key == 'ArrowDown') Keys.down = false;
-    if(e.key == 'ArrowLeft') Keys.left = false;
-    if(e.key == 'ArrowRight') Keys.right = false;
+    if(e.key == 'ArrowUp') Partida.snake.direccion = 'up'
+    if(e.key == 'ArrowDown') Partida.snake.direccion = 'down'
+    if(e.key == 'ArrowLeft') Partida.snake.direccion = 'left'
+    if(e.key == 'ArrowRight') Partida.snake.direccion = 'right'
+    console.log(Partida.snake.direccion)
 }) 
 
+// document.body.addEventListener('keydown', (e)=>{
+//     if(e.key == 'ArrowUp') Keys.up = true;
+//     if(e.key == 'ArrowDown') Keys.down = true;
+//     if(e.key == 'ArrowLeft') Keys.left = true;
+//     if(e.key == 'ArrowRight') Keys.right = true;
+// }) 
+// document.body.addEventListener('keyup', (e)=>{
+//     if(e.key == 'ArrowUp') Keys.up = false;
+//     if(e.key == 'ArrowDown') Keys.down = false;
+//     if(e.key == 'ArrowLeft') Keys.left = false;
+//     if(e.key == 'ArrowRight') Keys.right = false;
+// }) 
+
+const celdasX = 50
+const celdasY = 50
+const celdasW = Math.floor(canvas.width/celdasX)
+const celdasH = Math.floor(canvas.height/celdasY)
+
 const CanvasGrid = {
-    celdasX: 100,
-    celdasY: 100,
-    w: 10, //Math.floor(canvas.width/this.celdasX)
-    h: 10, //Math.floor(canvas.height/this.celdasY)
+    w: celdasW, //Math.floor(canvas.width/this.celdasX)
+    h: celdasH, //Math.floor(canvas.height/this.celdasY)
     draw(x,y,color) {
         ctx.fillStyle = color
         ctx.fillRect(x*this.w, y*this.h, this.w, this.h)
@@ -52,32 +63,67 @@ class Snake {
         CanvasGrid.draw(0,0,this.color)
     }
     move() {
-        if (this.direccion == 'right') {
-            let temp = [(this.arrayPosiciones[this.arrayPosiciones.length-1]),(this.arrayPosiciones[this.arrayPosiciones.length-1]+1)]
-            console.log(temp)
-            this.arrayPosiciones.push(temp)
+        let temp
+        if (this.direccion == 'up') {
+            this.y--
+            temp = [this.arrayPosiciones[this.arrayPosiciones.length-1][0],this.arrayPosiciones[this.arrayPosiciones.length-1][1]-1]
         }
+        if (this.direccion == 'down') {
+            this.y++
+            temp = [this.arrayPosiciones[this.arrayPosiciones.length-1][0],this.arrayPosiciones[this.arrayPosiciones.length-1][1]+1]
+        }
+        if (this.direccion == 'right') {
+            this.x++
+            temp = [this.arrayPosiciones[this.arrayPosiciones.length-1][0]+1,this.arrayPosiciones[this.arrayPosiciones.length-1][1]]
+        }
+        if (this.direccion == 'left') {
+            this.x--
+            temp = [this.arrayPosiciones[this.arrayPosiciones.length-1][0]-1,this.arrayPosiciones[this.arrayPosiciones.length-1][1]]
+        }
+        // console.log([Partida.comida.x,Partida.comida.y], Partida.snake.arrayPosiciones[Partida.snake.arrayPosiciones.length-1])
+        if(Partida.comida.x == Partida.snake.x && Partida.comida.y == Partida.snake.y) {
+            console.log('ñam')
+            this.longitud++
+            Partida.generaComida()
+        }
+        this.arrayPosiciones.push(temp)
+        if (this.arrayPosiciones.length > this.longitud) this.arrayPosiciones.shift()
+
+    }
+}
+class Comida {
+    x=0
+    y=0
+    color = 'white'
+    constructor(){
+        this.x= Math.floor(Math.random()*celdasX)
+        this.y= Math.floor(Math.random()*celdasY)
+    }
+    draw() {
+        CanvasGrid.draw(this.x,this.y,this.color)
     }
 }
 const Partida = {
     snake: new Snake,
+    puntos:0,
+    comida: new Comida(),
+    generaComida() {
+        this.comida = new Comida()
+    }
     
 }
-
+Partida
 //Refresco de canvas
 function update() {
-    ctx.clearRect(0,0,canvas.width,canvas.height)
     setInterval(()=>{
+        ctx.clearRect(0,0,canvas.width,canvas.height)
         // console.log(Partida.snake.arrayPosiciones)
+        Partida.comida.draw()
         Partida.snake.arrayPosiciones.forEach(e => {
             CanvasGrid.draw(e[0],e[1],Partida.snake.color)
         })
-        CanvasGrid.draw(0,0,Partida.snake.color)
         Partida.snake.move()
     },velocity)
 }
-setInterval(()=>{
-    
-},velocity)
 
 requestAnimationFrame(update)
