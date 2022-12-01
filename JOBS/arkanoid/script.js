6b0ad61d435f206b4b1393f4f9ffae7cbe079ef6
missed86 <jimmy11286@gmail.com>
2022-12-01 10:14:28 +0100

    asteroides try

--- JOBS/arkanoid/script.js
+++ JOBS/arkanoid/script.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext("2d")
 
+const velocity = 5;
+
 const nave = ({x=0, y=0}) => ({
     x,
     y,
@@ -12,12 +14,44 @@ const nave = ({x=0, y=0}) => ({
         ctx.fillRect(this.x, this.y, this.w, this.h)
     },
     move() {
-        if (Keys.up) this.y--
-        if (Keys.down) this.y++
-        if (Keys.left) this.x--
-        if (Keys.right) this.x++
+        if (Keys.up) this.y -= velocity
+        if (Keys.down) this.y += velocity
+        if (Keys.left) this.x -= velocity
+        if (Keys.right) this.x += velocity
+        outOfBounds(this.x, this.y)
     }
 })
+
+class asteroide {
+    w = 20
+    h = 20
+    color = 'red'
+    constructor() {
+        this.x = Math.random()*(canvas.width-this.w)
+        this.y = Math.random()*(canvas.height-this.h)
+    }
+    draw() {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.w, this.h)
+    }
+}
+const asteroides = []
+asteroides.push(new asteroide)
+asteroides.push(new asteroide)
+asteroides.push(new asteroide)
+asteroides.push(new asteroide)
+asteroides.push(new asteroide)
+asteroides.push(new asteroide)
+asteroides.push(new asteroide)
+asteroides.push(new asteroide)
+asteroides.push(new asteroide)
+asteroides.push(new asteroide)
+
+asteroides.forEach(e => {
+    e.draw();
+});
+
+
 const jugador = nave({})
 
 jugador.draw()
@@ -44,10 +78,21 @@ document.body.addEventListener('keyup', (e)=>{
     e.preventDefault()
 }) 
 
+function outOfBounds(x,y){
+    if(x <= 0) jugador.x = 0
+    if(x >= canvas.width-jugador.w) jugador.x = canvas.width - jugador.w 
+    if(y <= 0) jugador.y = 0
+    if(y >= canvas.height-jugador.h) jugador.y = canvas.height - jugador.h 
+}
+
 function update() {
     ctx.clearRect(0,0,canvas.width,canvas.height)
     jugador.draw()
     jugador.move()
     requestAnimationFrame(update)
+    asteroides.forEach(e => {
+        e.draw();
+    });
+    
 }
 requestAnimationFrame(update)
